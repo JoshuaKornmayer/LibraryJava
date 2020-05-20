4436a5703862f51d6b78031bf8936fe2f11bc74c
JoshuaKornmayer <jbkornmayer80@gmail.com>
2020-05-20 17:05:19 -0400

    fix newspaper constructor to match book

--- Newspaper.java
+++ Newspaper.java
@@ -1,8 +1,8 @@
 public class Newspaper extends Periodical {
 
     public Newspaper(String title, long yearReleased, long id, double costLostDamaged, String returnDate,
-            double lateFee, String issueDate, String newIssue) {
-        super(title, yearReleased, id, costLostDamaged, returnDate, lateFee, issueDate, newIssue);
+            double lateFee, boolean checkedOut, String issueDate, String newIssue) {
+        super(title, yearReleased, id, costLostDamaged, returnDate, lateFee, checkedOut, issueDate, newIssue);
 
     }
 }
