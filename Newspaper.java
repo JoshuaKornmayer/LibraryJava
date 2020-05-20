b49c21dd8d15ffd2ef8cd7037d0db70eca401a18
JoshuaKornmayer <jbkornmayer80@gmail.com>
2020-05-20 17:24:50 -0400

    added publisher to newspaper

--- Newspaper.java
+++ Newspaper.java
@@ -5,6 +5,7 @@ public class Newspaper extends Periodical {
     public Newspaper(String title, long yearReleased, long id, double costLostDamaged, String returnDate,
             double lateFee, boolean checkedOut, String genre, String issueDate, String newIssue, String publisher) {
         super(title, yearReleased, id, costLostDamaged, returnDate, lateFee, checkedOut, genre, issueDate, newIssue);
+        this.publisher = publisher;
 
     }
 }
